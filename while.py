e3d426e88d541865c2aff33f88ae5804da1e56c4
rubenhjo <rubenmhj@gmail.com>
2026-08-27 21:29:02 +0200

    ferre meldingert

--- while.py
+++ while.py
@@ -4,7 +4,5 @@ while True:
     print("Koding er gøy!!!")
     break
   else:
-    for i in range (0,9):
-              print(f"{a} Elsker koding!!")
     a = input ("Hvem elsker koding? (Skriv stopp for å stopp programmet)")
-      
+    print(f"{a} Elsker koding!!")
